5c0df4005a8db9c5716420006165845070c87ff0
CMAPAAPASEOGDE <cmapadev@outlook.com>
2025-07-28 14:44:15 -0600

    cambios en almacen - cajas

--- boxinspect.php
+++ boxinspect.php
@@ -218,13 +218,11 @@ if ($stmtContenido === false) {
                 echo '<div class="error-container">No se encontraron elementos en esta caja</div>';
             }
             ?>
-            
+
             <div class="nuevos-elementos" id="nuevos-elementos">
               <!-- Aquí se añadirán dinámicamente nuevos campos -->
             </div>
-        </section>
-
-        
+        </section>     
 
         <div class="caja-gestion-actions">
             <button type="button" class="btn-secundario" onclick="agregarElemento()">AÑADIR NUEVO ELEMENTO</button>
