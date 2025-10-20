80fef0b9d7921a9b9b840feaaa00733b644e1d81
CMAPAAPASEOGDE <cmapadev@outlook.com>
2025-10-20 10:06:23 -0600

    correcion error al anadir inventario

--- exstentry.php
+++ exstentry.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
